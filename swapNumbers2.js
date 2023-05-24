e4ecbed883d0057c3ba5e80e0243f56ef2ac6bf1
Tanishqa Arora <tanishqa164@gmail.com>
2023-05-25 01:09:24 +0530

    swap numbers using third number

---
+++ swapNumbers2.js
@@ -0,0 +1,16 @@
+// Program to swap two numbers
+// using third variable
+
+let a = 10;
+let b = 20;
+
+console.log(`Before Swapping: a = ${a} and b = ${b}`);
+
+let temp = a;
+a = b; 
+b = temp;
+
+console.log(`After Swapping: a = ${a} and b = ${b}`);
+
+
+
