9f0698f661ede547945008fd3063ec1679a92d16
Tanishqa Arora <tanishqa164@gmail.com>
2023-09-29 23:38:55 +0530

    update palindrome program

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,16 @@
-// check if the string is palindrome or not
+// check if the string is palindrome or not, considering spaces also like str = 'ab cd'
 
-// check using loop
+// replace(/\s/g, "") will use to remove any white spaces from string 
+// The \s meta character in js regex matches any whitespace character: spaces, tabs, newlines and Unicode spaces.
+// and the g flag tells js to replace it multiple times. If you miss g, it will only replace the first occurrence of the white space.
 
-let str = 'Abcba';
+// 1) using loop - matching start and end index
 
+let str = 'Abc b a';
 
 function checkPalindrome(string) {
-    // firstly convert string to lower case 
-    let newStr = string.toLowerCase();
+    // firstly remove spaces from string and convert it to lower case 
+    let newStr = string.replace(/\s/g, "").toLowerCase();
     let start = 0;
     let end = newStr.length-1;
 
@@ -26,10 +29,25 @@ function checkPalindrome(string) {
 
 console.log(checkPalindrome(str));
 
-// check using built-in functions
-let newStr = str.toLowerCase();
-let reverseStr = newStr.split('').reverse().join('');
-if( newStr === reverseStr ){
+// 2) using loop - comparing string with its reverse
+
+let newStr = str.replace(/\s/g, "").toLowerCase();
+let revStr = '';
+for(let i = newStr.length - 1; i >= 0; i--) {
+    revStr += newStr[i];
+}
+
+if(newStr === revStr) {
+    console.log("It is a Palindrome");
+} else {
+    console.log("It is not a Palindrome");
+}
+
+// 3) using built-in functions
+
+let reverseStr = str.replace(/\s/g, "").toLowerCase().split('').reverse().join('');
+
+if( str === reverseStr ){
     console.log('It is a Palindrome');
 }
 else{
