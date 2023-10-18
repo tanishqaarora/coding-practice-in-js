5275532e8740f5b9a2c3fe70d3343831f69d1e9e
Tanishqa Arora <tanishqa164@gmail.com>
2023-10-18 20:12:11 +0530

    count factors of a number

--- countFactors.js
+++ countFactors.js
@@ -1,10 +1,9 @@
 // Counting the number of factors of a number
-
 // Creating a function which will take a any number and counts its factors
 
 function countFactors(N) {
     count = 0;
-    for(let i = 1; i <= Math.sqrt(N); i ++) {
+    for(let i = 2; i <= Math.sqrt(N); i ++) {
         if(N % i == 0) {
             if(N/i == i) {
                 count ++ ;
