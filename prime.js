570fba3f3191d810241cfcefd4d43c1023de7559
Tanishqa Arora <tanishqa164@gmail.com>
2023-10-18 20:05:12 +0530

    check number is prime or not

--- prime.js
+++ prime.js
@@ -1,25 +1,20 @@
 // Program to check whether a number is prime or not
 
-let number = 11;
-
-function checkPrime(num) {
-    if(num <= 1) {
+function isPrime(num) {
+    if(num <=1 ) {
         return false;
     }
-    else if(num === 2) {
-        return true;
-    }
-    else {
+    else if(num > 1) {
         for(let i = 2; i <= Math.sqrt(num); i++) {
             if(num % i === 0) {
                 return false;
-            } 
+            }
         }
-        return true;
     }
+    return true;
 }
 
-const result = checkPrime(number);
+const result = isPrime(111);
 
 if(result) {
     console.log("It is a prime number.");
@@ -27,3 +22,24 @@ if(result) {
 else {
     console.log("It is not a prime number.")
 }
+
+
+// Find all the prime factors of the given number
+
+function factors(num) {
+    let factors = [];
+    let factor = 2;
+    while(num > 1) {
+        if(num % factor === 0) {
+            factors.push(factor);
+            num = num/factor;
+        } else {
+            factor++;
+        }
+    }
+    return factors;
+}
+factors(100);
+
+// to get unique factors;
+console.log([... new Set(factors(100))]);
