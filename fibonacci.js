c986d75356a815e0f17500e5f08db679488a2cb4
Tanishqa Arora <tanishqa164@gmail.com>
2023-10-18 20:00:39 +0530

    print fibonacci series

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,11 @@
 // Program to generate a fibonacci series up to certain terms
 
-let num = 8;
+let terms = 8;
 
+// initialize first two terms
 let n1 = 0, n2 = 1;
 
-for(let i = 0; i < num; i ++){
+for(let i = 0; i < terms; i ++){
     console.log(n1);
     let nextTerm = n1 + n2;
     n1 = n2;
